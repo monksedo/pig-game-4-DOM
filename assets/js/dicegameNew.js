cdefd2d36770d1ff371ad6b3d53c7ae5464c26d4
Chris Nguyen <chris@tnwebdev.com>
2020-02-14 19:26:17 -0800

    Update game logic in dicegameNew.js

--- assets/js/dicegameNew.js
+++ assets/js/dicegameNew.js
@@ -2,6 +2,15 @@
 // Roll The Dice Game - on ES6 and newer implementation
 //
 
+/*
+GAME RULES:
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
+
 // Set global veriables
 let scores = [0, 0];
 let activeScore, activePlayer;
@@ -21,6 +30,7 @@ document.querySelector('.dice').style.display = 'none';
 // Function - rollTheDice
 const rollTheDice = () => {
   if (gamePlaying) {
+    // Sellect current user
     let actScore = document.getElementById('current-' + activePlayer);
 
     // Get the dice random number
@@ -36,6 +46,7 @@ const rollTheDice = () => {
       // Add Score
       activeScore += dice;
       // actScore.textContent = activeScore;
+      console.log(activeScore);
     } else {
       switchPlayer();
     }
@@ -43,26 +54,8 @@ const rollTheDice = () => {
   }
 };
 
-// Function - switch players
-const switchPlayer = () => {
-  // Next Player
-  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
-  // Reset activeScore to 0
-  activeScore = 0;
-
-  // Change active player display
-  // player1.classList.remove('active');
-  // player2.classList.add('active');
-  player1.classList.toggle('active');
-  player2.classList.toggle('active');
-
-
-  // Change the dice display when the dice is 1
-  // diceImage.style.display = 'none';
-}
-
-
+// Update totalScore
 const updateTotalScore = () => {
   if (gamePlaying) {
     let totalScore = document.getElementById('score-' + activePlayer);
@@ -86,7 +79,31 @@ const updateTotalScore = () => {
       // Change player 
       switchPlayer();
     }
+    activeScore.textContent = 0;
   }
+}
+
+// Function - switch players
+const switchPlayer = () => {
+  // Sellect current user
+  let actScore = document.getElementById('current-' + activePlayer);
+
+  // Next Player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+  // Reset activeScore to 0
+  activeScore = 0;
+  actScore.textContent = activeScore;
+  console.log(activeScore);
+
+  // Change active player display
+  // player1.classList.remove('active');
+  // player2.classList.add('active');
+  player1.classList.toggle('active');
+  player2.classList.toggle('active');
+
+  // Change the dice display when the dice is 1
+  // diceImage.style.display = 'none';
 }
 
 // Reset all game score on load or function call
